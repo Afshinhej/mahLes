ea4c36d108a9167c4bcc287608f62b55bfd2bb74
Afshinhej <afshin.hejab@gmail.com>
2022-11-06 23:45:03 +0330

    All conflicts of models were removed.

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Seller(models.Model):
+    name = models.CharField(max_length=64)
+
 class Category(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
@@ -17,7 +20,7 @@ class Auction(models.Model):
     starting_bid = models.FloatField() #maybe it is beter to use models.IntegerField
     imageURL = models.URLField()
     category = models.ManyToManyField(Category, blank=True, related_name="category")
-    owner = models.ManyToManyField(User, related_name="owner")
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title}"
